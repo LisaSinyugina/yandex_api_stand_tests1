6cf1551e5728244ec3f96b263ddb037b75c6391a
Lisa <runand.go@yandex.ru>
2023-10-07 11:24:25 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+URL_SERVICE = "https://374ca4cc-b246-4b11-9e0a-a838076665da.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
+
